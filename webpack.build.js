aa882e800af77e0d41f68fdfafb5e4873fe8b991
zxing2021 <a18752140506@163.com>
2022-03-16 12:54:48 +0800

    2 files modified, 4 deleted

--- webpack.build.js
+++ webpack.build.js
@@ -1,78 +1,92 @@
-let path = require("path");
-let Clean = require("clean-webpack-plugin").CleanWebpackPlugin;
+let path = require('path');
+let Clean = require('clean-webpack-plugin').CleanWebpackPlugin;
 let Html = require('html-webpack-plugin');
-let {VueLoaderPlugin} = require('vue-loader');
+let { VueLoaderPlugin } = require('vue-loader');
 let css = {
-	loader: "css-loader",
-	options: {
-		importLoaders: 1,
-		esModule: false
-	}
-}
+  loader: 'css-loader',
+  options: {
+    importLoaders: 1,
+    esModule: false,
+  },
+};
 let postcss = {
-	loader: "postcss-loader",
-	options: {
-		postcssOptions: {
-			plugins: ["postcss-preset-env"]
-		}
-	}
-}
+  loader: 'postcss-loader',
+  options: {
+    postcssOptions: {
+      plugins: ['postcss-preset-env'],
+    },
+  },
+};
 let babel = {
-	loader: "babel-loader",
-	options: {
-		presets: [["@babel/preset-env", {
-			useBuiltIns: 'usage',
-			corejs: 3
-		}]]
-	}
-}
+  loader: 'babel-loader',
+  options: {
+    presets: [
+      [
+        '@babel/preset-env',
+        {
+          useBuiltIns: 'usage',
+          corejs: 3,
+        },
+      ],
+    ],
+  },
+};
 let file = {
-	type: "asset",
-	generator: {
-		filename: "file/[name][hash:6].[ext]"
-	}, parser: {
-		dataUrlCondition: {
-			maxSize: 24 * 1024,
-		}
-	}
-}
+  type: 'asset',
+  generator: {
+    filename: 'file/[name][hash:6].[ext]',
+  },
+  parser: {
+    dataUrlCondition: {
+      maxSize: 24 * 1024,
+    },
+  },
+};
 module.exports = {
-	mode: "production",
-	entry: "./src/index.js",
-	output: {
-		filename: "dist.js",
-		path: path.resolve(__dirname, "dist")
-	}, module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', css, postcss]
-			}, {
-				test: /\.less$/,
-				use: ['style-loader', css, postcss, 'less-loader']
-			}, {
-				test: /\.(png|ttf|woff|woff2|svg|eot)$/,
-				...file
-			}, {
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: [babel]
-			}, {
-				test: /\.vue$/,
-				use: ['vue-loader']
-			}
-		]
-	}, plugins: [
-		new Clean(),
-		new Html({
-			template: "./public/index.html"
-		}), new VueLoaderPlugin()
-	],
-	target: "web",
-	devServer: {
-		hot: true
-	},
-	performance: {
-		hints: false,
-	}
-}
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'build.js',
+    path: path.resolve(__dirname, 'build'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', css, postcss],
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', css, postcss, 'less-loader'],
+      },
+      {
+        test: /\.(png|ttf|woff|woff2|svg|eot)$/,
+        ...file,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [babel],
+      },
+      {
+        test: /\.vue$/,
+        use: ['vue-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new Clean(),
+    new Html({
+      template: './public/index.html',
+    }),
+    new VueLoaderPlugin(),
+  ],
+  target: 'web',
+  devServer: {
+    port: 3002,
+    hot: true,
+  },
+  performance: {
+    hints: false,
+  },
+};
