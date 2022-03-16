3f54b0ddca80035706ae3499c67b0567aa38bca9
zxing2021 <a18752140506@163.com>
2022-03-16 12:47:56 +0800

    1 file modified, 4 deleted

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import Vue from "vue/dist/vue.esm"
-import App from "./App.vue"
-import store from "./config/store"
-import router from "./config/router"
-import "./config/axios";
-import "font-awesome/css/font-awesome.css"
-import "./config/antd"
+import Vue from 'vue/dist/vue.esm';
+import App from './App.vue';
+import store from './config/store';
+import router from './config/router';
+import './config/axios';
+import './config/antd';
 let app = new Vue({
-	store, router,
-	render: h => h(App)
-}).$mount("#app")
+  store,
+  router,
+  render: (h) => h(App),
+}).$mount('#app');
